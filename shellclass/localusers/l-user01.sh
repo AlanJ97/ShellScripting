55075312f92849fd5f231fd10b4f27ff6f5a04e4
Alan <alannavaseg_02@hotmail.com>
2020-08-25 19:36:10 -0500

    TErminando el primer script de cosas basicas

--- shellclass/localusers/l-user01.sh
+++ shellclass/localusers/l-user01.sh
@@ -4,3 +4,24 @@
 
 #Say HELLO
 echo "hello"
+#type -a "ORDER" -> this gives you informationa bout the order in the system
+
+#Assign a value to a variable
+WORD='script'
+ENDING='ed'
+
+#print things
+echo "$WORD"
+echo '$WORD'
+echo "This is a shell $WORD"
+echo "This also is a shell ${WORD}"
+
+#Append text to a variable with ${}
+echo "${WORD}ing is fun!"
+
+#Combining two or more variables
+echo "The combination is ${WORD}${ENDING}"
+
+#Reassigning values in variables
+ENDING='ing'
+echo "The combination is ${WORD}${ENDING}"
