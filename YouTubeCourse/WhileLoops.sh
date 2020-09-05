5750c93aee318c2866127ee002d24304e2d9714f
Alan <alannavaseg_02@hotmail.com>
2020-09-04 19:44:23 -0500

    Learning sleep and open terminals

--- YouTubeCourse/WhileLoops.sh
+++ YouTubeCourse/WhileLoops.sh
@@ -10,10 +10,14 @@ done
 m=1
 
 #This is a second way to solve this
+#sleep dalays in seconds given the function
+#gnome-terminal & this opens terminal if needed
 while (( "$m" <= 10 )) 
 do
     echo "$m"
     m=$(( ++m ))
+    sleep 2
+    gnome-terminal &
 done
 
 
